ad23be519a235dd7c8096b93e8f82ab328b26c38
Eric Xu <ericxu@ucsb.edu>
2022-01-10 21:25:50 -0800

    EX: Totally misunderstood for the requirement for what needs to be printed I thoght it was netID but it was actually the school email front

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from ericxu!";
+        return "Greetings from ericxu002!";
     }
     
 }
